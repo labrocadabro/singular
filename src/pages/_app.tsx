4b0cf815981d79e1604402cde108e30461ec1440
Laura Abro <l.abrocadabro@gmail.com>
2023-04-20 15:18:48 +0500

    switch to t3 stack and clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,14 @@
-import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
 
-import { SessionProvider } from "next-auth/react";
-
-export default function App({
-	Component,
-	pageProps: { session, ...pageProps },
-}: AppProps) {
-	return (
-		<SessionProvider session={session}>
-			<Component {...pageProps} />
-		</SessionProvider>
-	);
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
+
+export default api.withTRPC(MyApp);
