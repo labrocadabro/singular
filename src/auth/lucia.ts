0d78ab12417a880e59269eefa0b02ae03bdb714a
Laura Abro <l.abrocadabro@gmail.com>
2023-05-11 09:53:34 +0500

    remove old code

--- src/auth/lucia.ts
+++ src/auth/lucia.ts
@@ -6,20 +6,7 @@ import { PrismaClient } from "@prisma/client";
 import { github } from "@lucia-auth/oauth/providers";
 
 let db;
-// if (process.env.NODE_ENV === "production") {
 db = new PrismaClient();
-// } else {
-//   //check if there is already a connection to the database
-//   /* eslint-disable */
-//   // @ts-ignore
-//   if (!global.db) {
-//     // @ts-ignore
-//     global.db = new PrismaClient();
-//   }
-//   // @ts-ignore
-//   db = global.db;
-// }
-// /* eslint-enable */
 
 export const auth = lucia({
 	adapter: prisma(db),
